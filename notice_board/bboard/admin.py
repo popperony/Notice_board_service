1a37a988f557e7e3b2d2e788c0e4c6e03adf5de0
Вадим <popperony@yandex.ru>
2021-02-26 17:18:50 +0300

    создание общей структуры проекта

--- notice_board/bboard/admin.py
+++ notice_board/bboard/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bb, Category
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'category')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+# Class Category(admin.ModelAdmin):
+
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Category)
--- notice_board/bboard/views.py
+++ notice_board/bboard/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Bb
+from .models import Bb, Category
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    bboards = Bb.objects.order_by('-published')
-    context = {'bboards': bboards}
-    return HttpResponse(template.render(context, request))
+    bboards = Bb.objects.all()
+    category = Category.objects.all()
+    context = {'bboards': bboards, 'category': category}
+    return render(request, 'index.html', context)
+
+
+def category(request, category_id):
+    bboards = Bb.objects.filter(category=category_id)
+    category = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+    context = {'bboards': bboards, 'category': category, 'current_category': current_category}
+    return render(request, 'category.html', context)
